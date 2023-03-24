0baac0666d3aa0821db3bb8936d9663b7fbe7b5e
ponomarev-vitaly <vp211082@gmail.com>
2023-03-25 01:32:13 +0300

    I created new Plane class and added new variables engineType, numberOfWings. I added toString method in order to change the car to plane in the output.

--- src/Plane.java
+++ src/Plane.java
@@ -1,2 +1,37 @@
-public class Plane {
+public class Plane extends Vehicle{
+    private String engineType;
+    private int numberOfWings;
+
+    public String getEngineType() {
+        return engineType;
+    }
+
+    public void setEngineType(String engineType) {
+        this.engineType = engineType;
+    }
+
+    public int getNumberOfWings() {
+        return numberOfWings;
+    }
+
+    public void setNumberOfWings(int numberOfWings) {
+        this.numberOfWings = numberOfWings;
+    }
+
+    public Plane(String VIN, String brand, String model, int NOW, int NOP, String color, String engineType, int numberOfWings) {
+        super(VIN, brand, model, NOW, NOP, color);
+        this.engineType = engineType;
+        this.numberOfWings = numberOfWings;
+    }
+
+    public Plane(){
+
+    }
+
+    public String toString(){
+        String temp = super.toString();
+        temp += "The plane has " + engineType + " engine and has number of " + numberOfWings + " wings. ";
+        String temp2 = temp.replace("car", "plane");
+        return temp2;
+    }
 }
