c830a416c1af37d360fdf63fb06d8ce49014fbd4
ponomarev-vitaly <vp211082@gmail.com>
2023-03-27 23:55:07 +0300

    If flag is false, If body runs. If we use ! mark before the variable, the value of it going to be that we initialise. If flag is true, this code runs. If flag1 is false, If block runs. != we use only for integers, but for other data types we use !

---
+++ src/IfStatementPractice.java
@@ -0,0 +1,26 @@
+public class IfStatementPractice {
+    public static void main(String[] args) {
+//        boolean flag = false;
+//        // If flag is false, If body runs.
+//        if(!flag){ // If we use ! mark before the variable, the value of it going to be that we initialised.
+//            System.out.println("The value of the flag is: " + flag);
+//        } else System.out.println("In other cases the value is: " + flag); // If flag is true, this code runs.
+//
+//        boolean flag1 = true;
+//        // If flag1 is false, If block runs.
+//        if(!flag1){
+//            System.out.println("The value of the flag is: " + flag1);
+//        } else System.out.println("In other cases the value is: " + flag1); // If flag1 is true, else body runs.
+//
+//        boolean flag2 = true;
+//        if (flag2 != false) { // != is an old example of compare operators.
+//            // != we use only for integers, but for other data types we use !
+//            System.out.println("The value of flag2 is " + flag2);
+//        } else System.out.println("There's STH else: " + flag2);
+
+        String flag4 = "Flag";
+        if(flag4 != "flag"){
+            System.out.println("The result is!");
+        } else System.out.println("Don't run this code!");
+    }
+}
