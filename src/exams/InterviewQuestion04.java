1aed0f0678b791d8a4825a714940048175544417
ponomarev-vitaly <vp211082@gmail.com>
2023-04-19 23:01:47 +0300

    I renamed the repository.

--- src/exams/InterviewQuestion04.java
+++ src/exams/InterviewQuestion04.java
@@ -5,20 +5,42 @@ import java.util.Scanner;
 public class InterviewQuestion04 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter a positive integer to check if it is prime or not");
 		int number = scan.nextInt();
 		primeCalculator(number);
         scan.close();
 
+		// Solution 2.
+		System.out.println("-----*********/////*********-----");
+		boolean flag = true;
+
+		if(number >= 2){
+			for (int i = 2; i < number; i++) {
+				if(number%i==0) {
+					flag = false;
+					break;
+				}
+			}
+
+			if(flag){
+				System.out.println("Number is prime " + number);
+			}else{
+				System.out.println("Number is not prime " + number);
+			}
+		}else if(number==1){
+			System.out.println("The number is prime " + number);
+		}else{
+			System.out.println("Number is not prime!");
+		}
 	}
-	
+
 	public static void primeCalculator(int number) {
 		int count = 0;
 		if(number>0) {
-			if (number == 1) { 
-				System.out.println(number + " is prime number"); 
+			if (number == 1) {
+				System.out.println(number + " is prime number");
 			}else {
 				for(int i=2; i<number; i++) {
 					if(number%i==0) {
@@ -34,8 +56,6 @@ public class InterviewQuestion04 {
 		}else {
 			System.out.println("Enter a positive integer");
 		}
-		
-
 	}
 
 }
